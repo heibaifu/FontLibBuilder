b8a619791d4c01e87e90d41df0e1b1628fa44909
t123yh <t123yh@outlook.com>
2016-06-10 10:42:14 +0800

    添加 CRC8 计算代码。

--- FontLibBuilder/CommonUtils.cs
+++ FontLibBuilder/CommonUtils.cs
@@ -27,5 +27,42 @@ namespace FontLibBuilder
             bits.CopyTo(ret, 0);
             return ret;
         }
+
+        static byte[] table = new byte[256];
+        // x8 + x7 + x6 + x4 + x2 + 1
+        const byte poly = 0xd5;
+
+        public static byte ComputeCrc8(params byte[] bytes)
+        {
+            byte crc = 0;
+            if (bytes != null && bytes.Length > 0)
+            {
+                foreach (byte b in bytes)
+                {
+                    crc = table[crc ^ b];
+                }
+            }
+            return crc;
+        }
+
+        static CommonUtils()
+        {
+            for (int i = 0; i < 256; ++i)
+            {
+                int temp = i;
+                for (int j = 0; j < 8; ++j)
+                {
+                    if ((temp & 0x80) != 0)
+                    {
+                        temp = (temp << 1) ^ poly;
+                    }
+                    else
+                    {
+                        temp <<= 1;
+                    }
+                }
+                table[i] = (byte)temp;
+            }
+        }
     }
 }
